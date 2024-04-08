9bd1983a23a2c9f5df014505dd925c0d2985a989
Andre Bothke <andrebothke@gmail.com>
2024-04-08 12:42:27 +0200

    route komplett

--- backend/controller/interiors.js
+++ backend/controller/interiors.js
@@ -1,48 +1,92 @@
-import express from 'express'
-import multer from 'multer'
-import { Interior } from '../models/interior.js'
+import express from "express";
+import multer from "multer";
+import { Interior } from "./../models/interior.js";
 
-const router = express.Router()
-const mult = multer()
+const router = express.Router();
+const mult = multer();
 
-router.get('/', async (req, res) => {
-    try {
-        const interiors = await Interior.find().lean()
-        res.json(interiors)
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(404)
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const interiors = await Interior.find().lean();
+    res.json(interiors);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(404);
+  }
+});
 
-router.get('/:category', async (req, res) => {
-    console.log(req.params.category);
-    try {
-        const interiors = await Interior.find({
-            "category": req.params.category,
-        }).lean()
-        res.json(interiors)
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(404)
-    }
-})
+router.get("/:category", async (req, res) => {
+  const category = req.params.category;
+  try {
+    const interiors = await Interior.find({
+      category: category.toString(),
+    }).lean();
+    res.json(interiors);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(404);
+  }
+});
 
-router.post('/', mult.none(), async (req, res) => {
-    try {
-        const newInterior = new Interior ({
-            title: req.body.title,
-            room: req.body.room,
-            image: req.body.image,
-            description: req.body.description,
-            category: req.body.category
-        })
-        const result = await newInterior.save()
-        res.status(201).json(result)
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(500)
-    }
-})
+router.post("/", mult.none(), async (req, res) => {
+  try {
+    const newInterior = new Interior({
+      title: req.body.title,
+      room: req.body.room,
+      image: req.body.image,
+      description: req.body.description,
+      category: req.body.category,
+    });
+    const result = await newInterior.save();
+    res.status(201).json(result);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
-export default router
+router.get("/details/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const interior = await Interior.findById(id).lean();
+    res.json(interior);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+router.patch("/details/:id", mult.none(), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const interior = await Interior;
+    const filter = { _id: id };
+    const update = {
+      title: req.body.title,
+      room: req.body.room,
+      image: req.body.image,
+      description: req.body.description,
+      category: req.body.category,
+    };
+    let doc = await interior.findOneAndUpdate(filter, update);
+    res.json(update);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+router.delete("/details/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const Interior = await Interior;
+    const filter = { _id: id };
+    let doc = await interior.findOneAndDelete(filter);
+    res.json(doc);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+export default router;
--- backend/models/interior.js
+++ backend/models/interior.js
@@ -1,26 +1,26 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose";
 
-const interiorSchema = new Schema ({
-    title: {
-        type: String,
-        required: true
-    },
-    room: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        // required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
-    }
-})
+const interiorSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  room: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    // required: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
 
-export const Interior = mongoose.model('Interior', interiorSchema)
+export const Interior = mongoose.model("Interior", interiorSchema);
